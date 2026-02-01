747e1c4371313c0c915d2f7414e240486efdc2cf
SreekanthMadhava75 <140582551+SreekanthMadhava75@users.noreply.github.com>
2026-02-01 15:18:17 +0530

    Change S3 region from ap-south-1 to eu-north-1

--- weekly_scraper.py
+++ weekly_scraper.py
@@ -65,7 +65,7 @@ def scrape_rasi(item):
     }
 
 def upload_to_s3(data):
-    s3 = boto3.client("s3", region_name="ap-south-1")
+    s3 = boto3.client("s3", region_name="eu-north-1")
     s3.put_object(
         Bucket=BUCKET_NAME,
         Key=S3_KEY,
